8c82f9c1f08f9ffa0a5fffa628904ca2a09c7777
Supachai Chaimangua (Tor) <tortechnocom@gmail.com>
2018-07-03 11:02:52 +0700

    Update root path

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HomepagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get homepages_index_url
+    get homepages_url
     assert_response :success
   end
 
